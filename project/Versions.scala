1eb7360823371453980c85fab738bb8ed3add29a
Romain Ruetschi <github@romac.me>
2026-03-29 19:04:12 +0200

    Migrate build from sbt to Mill 1.1.5

--- project/Versions.scala
+++
@@ -1,8 +0,0 @@
-object Versions {
-  val choreo          = "0.1.0"
-  val scala3          = "3.8.2"
-  val cats            = "2.13.0"
-  val catsEffect      = "3.6.3"
-  val munit           = "1.1.1"
-  val munitCatsEffect = "2.1.0"
-}
--- project/plugins.sbt
+++
@@ -1,3 +0,0 @@
-addSbtPlugin("com.github.sbt" % "sbt-github-actions" % "0.29.0")
-addSbtPlugin("org.scoverage"  % "sbt-scoverage"      % "2.4.4")
-addSbtPlugin("com.github.sbt" % "sbt-ci-release"     % "1.9.0")
